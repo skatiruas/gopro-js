7ea3b1ce71d072f24f8055213c2441116706eec1
Tiago Ruas <skatiruas@gmail.com>
2017-11-15 01:36:09 -0200

    Delegate function calls to api and fix arguments

--- src/GpControlAPI.js
+++ src/GpControlAPI.js
@@ -3,15 +3,20 @@ import axios from 'axios'
 const str = (str, prefix = '') => str ? `${prefix}${str}` : ''
 
 export default class GpControlAPI {
-  constructor(ip, mac) {
+  constructor({ ip, mac } = {}) {
     this.ip = ip || '10.5.5.9'
     this.mac = mac || 'AA:BB:CC:DD:EE:FF'
   }
 
-  request(path, port) {
-    const url = `http://${this.ip}${str(port, ':')}/gp/gpControl${str(path, '/')}`
-    return axios.get(url, { timeout: 5000 })
+  request(path, endpoint = 'gpControl', port = '') {
+    const url = `http://${this.ip}${str(port, ':')}/gp/${endpoint}${str(path, '/')}`
+    return axios.get(url, { timeout: 5000 }).then(r => r.data)
   }
 
+  _command(path) { return this.request(`command${str(path, '/')}`)}
+
   status() { return this.request('status') }
+  mode(mode, submode = 0) {
+    return this._command(`sub_mode?mode=${mode}&sub_mode=${submode}`)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 import GpControlAPI from './GpControlAPI'
 
-const TYPES = {
-  gpControl: /HERO4|HERO5|HERO\+/g,
-  auth: /HERO2|HERO3/g
+const REGEX = {
+  gpControl: /HERO4|HERO5|HERO\+/,
+  auth: /HERO2|HERO3/,
+  interface: /^status|mode|$/
 }
 
 export default class GoPro {
-  constructor({ ip, mac, model } = {}) {
-    this.ip = ip || '10.5.5.9'
-    this.mac = mac || 'AA:BB:CC:DD:EE:FF'
-    this._setAPI(this._selectAPI(model))
+  constructor(props = {}) {
+    this.apiProps = {
+      ip: props.ip || '10.5.5.9',
+      mac: props.mac || 'AA:BB:CC:DD:EE:FF',
+    }
+    this._setAPI(this._selectAPI(props.model))
+
+    // Delegate functions to API
+    return new Proxy(this, {
+      get: (target, property) => {
+        if (target[property] !== undefined) return target[property]
+        return (...args) => {
+          if (props.strict && !property.match(REGEX.interface)) {
+            return Promise.reject(`${property} not allowed on this interface.`)
+          } else return target.api[property].apply(target.api, args)
+        }
+      }
+    })
   }
 
   _selectAPI(model) {
     return (model && (
-      (model.match(TYPES.gpControl) && new GpControlAPI(this.ip, this.mac)) ||
-      (model.match(TYPES.auth) && null))) || undefined
+      (model.match(REGEX.gpControl) && new GpControlAPI(this.apiProps)) ||
+      (model.match(REGEX.auth) && null))) || undefined
   }
 
   _discover() {
-    let api = new GpControlAPI(this.ip, this.mac)
-    return api.request().then(r => {
-      const { model_name } = r.data.info
-      if (!model_name.match(TYPES.gpControl)) api = null
+    let api = new GpControlAPI(this.apiProps)
+    return api.request().then(data => {
+      this.apiProps['gpControlResponse'] = data
+      const { model_name } = data.info
+      if (!model_name.match(REGEX.gpControl)) api = null
       this._setAPI(api)
     }).catch(() => {
       // Handle other types later
@@ -34,20 +50,22 @@ export default class GoPro {
     this.api = new Proxy(api || {}, {
       get: (target, property) => {
         if (target[property] !== undefined) return target[property]
-        return () => {
-          if (api === null) return Promise.reject('GoPro not found.')
-          else if (api === undefined) {
-            return this._discover().then(() => this.api[property]())
-          } else {
-            return Promise.reject(`${property} not defined for current API.`)
-          }
+        return (...args) => {
+          if (api === undefined) {
+            const retry = () => this.api[property].apply(this.api, args)
+            return this._discover().then(retry)
+          } else if (api === null) return Promise.reject('GoPro not found.')
+          else return Promise.reject(`${property} not defined for current API.`)
         }
       }
     })
   }
 
-  status() { return this.api.status() }
+/* === Interface ===
+ * status()
+ * mode(mode, submode)
+ */
 }
 
 const x = new GoPro()
-x.status().then(r => console.log(r))
+x.mode(2).then(x.status).then(r => console.log(r))
