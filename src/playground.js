1779c3e3cfb2661aa8a26cb6050a3399814cf24d
Tiago Ruas <skatiruas@gmail.com>
2017-11-21 00:51:33 -0200

    Redesing proxy to improve usage and remove chaining bugs

--- src/playground.js
+++ src/playground.js
@@ -4,14 +4,33 @@ const gp = new GoPro() // Instantiate
 gp.mode('VIDEO') // Begin chaining (without then)
   .delay(1000) // Delays the next method
   .shutter(0, 2000) // Records 2s video after 0s delay
-  .then(() => gp.mode('MULTISHOT', 'BURST')) // Chaining with then
+  .status('Storage.RemainingPhotos')
+  .then(instance => { // Chaining with then **ATTENTION**
+    // Get result of last execution using instance.lastResult
+    const result = instance.lastResult
+    console.log(`RemainingPhotos: ${result}`)
+    // THEN and CATCH returns instance of GoPro to be used
+    // DON'T use gp here, as it will break the chain!!
+    if (result > 100) instance.mode('MULTISHOT', 'BURST')
+    else instance.mode('PHOTO', 'NIGHT')
+    instance.delay(5000)
+  })
   .dummy() // Calls and unexistent method, throwing an error
-  .catch(() => gp.mode('PHOTO', 'SINGLE')) // Catches the error and fallbacks to other mode
+  .catch(instance => {
+    console.log(instance.lastResult) // dummy not defined for current API.
+    // Catches the error and changes to other mode
+    instance.mode('PHOTO', 'SINGLE')
+  })
   .set('Photo.Resolution.R5MEDIUM') // Apply some setting
   .status('System.BUSY') // Check camera status
-  .then(b => console.log(`Camera is ${b ? 'BUSY' : 'OK'}`)) // Print previous result
+  .then(instance => {
+    console.log(`Camera is ${instance.lastResult ? 'BUSY' : 'OK'}`)
+  }) // Print previous result
   .shutter(2000) // Activate shutter after 2 seconds delay
   .set('Photo.Resolution.R12WIDE') // Apply some setting
-  .listMedia().then(m => console.log(m)) // Get JSON of media
+  .listMedia()
+  .then(({ lastResult }) => { // Accessing last result with Destructuring
+    console.log(lastResult)
+  })
   .deleteLast(2) // Deletes the last 2 files
   .powerOff() // Power Off
